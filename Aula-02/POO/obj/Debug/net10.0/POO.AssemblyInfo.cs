3218fd260ed898905c77688954ed3bc6821b540a
Prof. Cristiano de Paula <prof.cristianodepaula@gmail.com>
2026-07-29 16:40:09 -0300

    Aula 02 - Classe e Objeto

--- Aula-02/POO/obj/Debug/net10.0/POO.AssemblyInfo.cs
+++ Aula-02/POO/obj/Debug/net10.0/POO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4833573ca4e84912b1d9e51c8253c4d2cda31ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eca357de677d23135fdd3f2a3f5c7600827109c")]
 [assembly: System.Reflection.AssemblyProductAttribute("POO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
