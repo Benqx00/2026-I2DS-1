b3cd7635fc044425b171b0ac11404638636a4568
Prof. Cristiano de Paula <prof.cristianodepaula@gmail.com>
2026-08-04 16:27:09 -0300

    Lista de Exercicios

--- Aula-03/Condicionais/obj/Debug/net10.0/Condicionais.AssemblyInfo.cs
+++ Aula-03/Condicionais/obj/Debug/net10.0/Condicionais.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Condicionais")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a997697756f1c3709372acbb5b77d825db756f0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a1dfb7ce767c2330071079edd5a77f11713ea90")]
 [assembly: System.Reflection.AssemblyProductAttribute("Condicionais")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Condicionais")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Aula-03/Funcao/obj/Debug/net10.0/Funcao.AssemblyInfo.cs
+++ Aula-03/Funcao/obj/Debug/net10.0/Funcao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Funcao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a997697756f1c3709372acbb5b77d825db756f0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a1dfb7ce767c2330071079edd5a77f11713ea90")]
 [assembly: System.Reflection.AssemblyProductAttribute("Funcao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Funcao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
