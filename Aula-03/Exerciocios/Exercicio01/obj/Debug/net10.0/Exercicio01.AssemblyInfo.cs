a997697756f1c3709372acbb5b77d825db756f0f
Prof. Cristiano de Paula <prof.cristianodepaula@gmail.com>
2026-08-04 16:22:56 -0300

    Correção do nome da pasta

--- Aula-03/Exerciocios/Exercicio01/obj/Debug/net10.0/Exercicio01.AssemblyInfo.cs
+++ Aula-03/Exerciocios/Exercicio01/obj/Debug/net10.0/Exercicio01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01d70945902217aa6ed15ed4a8e69521d980d09f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dff08a4c3fafb06afdb02b33a3375069d760c31")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
